6d4e5da4bbb7262f4115e9532d1da6e56cfe4946
Youssef ALMIA <almia.youssef2@gmail.com>
2023-10-31 14:06:10 +0100

    fixed index

--- src/index.js
+++ src/index.js
@@ -1,47 +1,45 @@
 import React, { Component, Suspense } from "react";
 import ReactDOM from "react-dom";
 
-import './main.scss';
+import "./main.scss";
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 import Load from "./components/Load";
-import ProductList from './pages/product/ProductList';
+import ProductList from "./pages/product/ProductList";
 import Postview from "./components/Postview";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 const SkillList = React.lazy(() => import("./pages/skill/SkillList"));
 const EventList = React.lazy(() => import("./pages/event/EventList"));
 const JobList = React.lazy(() => import("./pages/Job"));
 const QuestionList = React.lazy(() => import("./pages/skill/QuestionList"));
 const AnswerList = React.lazy(() => import("./pages/skill/AnswerList"));
-const UserList = React.lazy(() => import("./pages/UserList"));
+// const UserList = React.lazy(() => import("./pages/UserList"));
 const QuizList = React.lazy(() => import("./pages/skill/QuizList"));
 const UserList = React.lazy(() => import("./pages/user/UserList"));
 const Home = React.lazy(() => import("./pages/Home"));
 
 class Root extends Component {
-    render() {
-        return (
-            <BrowserRouter basename={'/'}>
-                <Suspense fallback={<Load/>}>
-                    <Routes>
-                        <Route path={`/skills`} element={<SkillList/>}/>
-                        <Route path={`/events`} element={<EventList/>}/>
-                        <Route path={`/jobs`} element={<JobList/>}/>
-                        <Route path={`/questions`} element={<QuestionList/>}/>
-                        <Route path={`/products`} element={<ProductList />} />
-                        <Route path={`/answers`} element={<AnswerList/>}/>
-                        <Route path={`/users`} element={<UserList/>}/>
-
-                        <Route path={`/quizzes`} element={<QuizList/>}/>
-                        <Route path={`/users`} element={<UserList/>}/>
-                        <Route path={`/posts`} element={<Home />} />
-                    </Routes>
-                </Suspense>
-            </BrowserRouter>
-        )
-    }
+  render() {
+    return (
+      <BrowserRouter basename={"/"}>
+        <Suspense fallback={<Load />}>
+          <Routes>
+            <Route path={`/skills`} element={<SkillList />} />
+            <Route path={`/events`} element={<EventList />} />
+            <Route path={`/jobs`} element={<JobList />} />
+            <Route path={`/questions`} element={<QuestionList />} />
+            <Route path={`/products`} element={<ProductList />} />
+            <Route path={`/answers`} element={<AnswerList />} />
+            <Route path={`/quizzes`} element={<QuizList />} />
+            <Route path={`/users`} element={<UserList />} />
+            <Route path={`/posts`} element={<Home />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    );
+  }
 }
 
 ReactDOM.render(<Root />, document.getElementById("root"));
